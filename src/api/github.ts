14f4e42450a268de161d010cb7f4fb4bab0e83de
tienle02 <thuytien.le1103@gmail.com>
2026-02-16 23:50:55 -0600

    add user suggestions when search

--- src/api/github.ts
+++ src/api/github.ts
@@ -9,3 +9,15 @@ export const fetchGithubUser = async (username: string) => {
   console.log(data);
   return data;
 };
+
+export const SearchGithubUser = async (query: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}`,
+  );
+
+  if (!res.ok) throw new Error("User not found");
+
+  const data = await res.json();
+  console.log(data);
+  return data.items;
+};
--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { fetchGithubUser } from "../api/github";
+import { fetchGithubUser, SearchGithubUser } from "../api/github";
 import UserCard from "./UserCard";
 import RecentSearches from "./RecentSearch";
+import { useDebounce } from "use-debounce";
+import type { GithubUser } from "../types";
 
 const UserSearch = () => {
   const [userName, setUsername] = useState("");
@@ -12,11 +14,22 @@ const UserSearch = () => {
     return stored ? JSON.parse(stored) : [];
   });
 
+  const [debouncedUsername] = useDebounce(userName, 300); // Wait 300ms before making request
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  // Query to fetch specific user
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users", submittedUsername],
     queryFn: () => fetchGithubUser(submittedUsername),
     // !! turn into a boolean
     enabled: !!submittedUsername,
+  });
+
+  // Query to fetch suggestions for user search
+  const { data: suggestions } = useQuery({
+    queryKey: ["github-user-suggestions", debouncedUsername],
+    queryFn: () => SearchGithubUser(debouncedUsername),
+    enabled: debouncedUsername.length > 1,
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -40,12 +53,35 @@ const UserSearch = () => {
   return (
     <>
       <form onSubmit={handleSubmit} className="form">
-        <input
-          type="text"
-          placeholder="Enter Github Username..."
-          value={userName}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <div className="dropdown-wrapper">
+          <input
+            type="text"
+            placeholder="Enter Github Username..."
+            value={userName}
+            onChange={(e) => {
+              const val = e.target.value;
+              setUsername(val);
+              setShowSuggestions(val.trim().length > 1);
+            }}
+          />
+          {showSuggestions && suggestions?.length > 0 && (
+            <ul className="suggestions">
+              {suggestions.slice(0, 5).map((user: GithubUser) => (
+                <>
+                  <li key={user.login}>
+                    <img
+                      src={user.avatar_url}
+                      alt={user.login}
+                      className="avatar-xs"
+                    />{" "}
+                    {user.login}
+                  </li>
+                </>
+              ))}
+            </ul>
+          )}
+        </div>
+
         <button type="submit">Search</button>
       </form>
 
